d2eff6d5fcbbe3cc76f4adce4adb060a2f347d8a
vlnnns <102325878+vlnnns@users.noreply.github.com>
2024-11-08 08:33:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,27 @@ terraform {
   required_providers {
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
+      version = "~> 2.24.0"
     }
   }
 }
 
 provider "docker" {}
 
-resource "docker_image" "my_docker_image" {
-  name         = "vlnnns/frontend:latest"  # Путь к образу Docker Hub
+
+resource "docker_registry_image" "frontend_image" {
+  name = "vlnnns/frontend:latest" 
+  build {
+    context    = "./"
+    dockerfile = "Dockerfile-Frontend"
+  }
+  
   keep_locally = false
 }
 
-resource "docker_container" "my_container" {
-  name  = "my_container_name"
-  image = docker_image.my_docker_image.latest
+resource "docker_container" "frontend_container" {
+  name  = "frontend_container"
+  image = docker_registry_image.frontend_image.name
 
   ports {
     internal = 80
@@ -28,4 +34,3 @@ resource "docker_container" "my_container" {
     "ENV_VAR2=value2"
   ]
 }
-
